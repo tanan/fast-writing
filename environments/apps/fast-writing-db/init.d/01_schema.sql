4fe1e003b399d84341e6b4d67e78bba9400ab489
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-03-05 23:00:21 +0900

    fix schema

--- environments/apps/fast-writing-db/init.d/01_schema.sql
+++ environments/apps/fast-writing-db/init.d/01_schema.sql
@@ -6,9 +6,9 @@ USE fast_writing;
 
 create table user
 (
-  id binary(16) primary key default (UUID_TO_BIN(UUID(), TRUE)),
+  id binary(16) primary key default (UUID_TO_BIN(UUID())),
   name varchar(256) not null,
   email varchar(256) not null,
-  last_updated datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  last_updated datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 )
 charset=utf8mb4;
--- environments/apps/fast-writing-db/run.sh
+++ environments/apps/fast-writing-db/run.sh
@@ -2,12 +2,10 @@
 
 app="fast-writing-db"
 tag=${1:-"latest"}
-docker_network=${2:-"fast-writing"}
 image_name="gcr.io/anan-project/fast-writing/${app}:${tag}"
 
 [[ `docker ps -a -f name=${app} -q | wc -l` -eq 1 ]] && docker rm -f ${app}
 docker run -d -p 3306:3306 \
     -e MYSQL_ROOT_PASSWORD=contoro \
-    --network=${docker_network} \
     --name ${app} \
     ${image_name}
