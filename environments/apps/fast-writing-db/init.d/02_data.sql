83a6b728b0ae4e1149f39c01483e3f829bc2cdbc
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-03-06 15:33:18 +0900

    db: fix init data

--- environments/apps/fast-writing-db/init.d/02_data.sql
+++ environments/apps/fast-writing-db/init.d/02_data.sql
@@ -1,4 +1,4 @@
 use fast_writing;
 insert into user values
-(UUID_TO_BIN('11eae085-55e6-e2ca-a15d-0242ac110002'), 'test1', 'test1@example.com')
+(UUID_TO_BIN('11eae085-55e6-e2ca-a15d-0242ac110002'), 'test1', 'test1@example.com', CURRENT_TIMESTAMP())
 ;
