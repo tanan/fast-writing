288ca3b4bd090cf6745568ecbbaf3034d063b634
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-03-07 21:18:55 +0900

    api: add writing domain

---
+++ apps/fast-writing-api/domain/writing_contents.go
@@ -0,0 +1,20 @@
+package domain
+
+import "time"
+
+type ContentsId int64
+
+type Contents struct {
+	ContentsId  ContentsId
+	Creator     string
+	Title       string
+	QuizList    []Quiz
+	LastUpdated time.Time
+}
+
+type Quiz struct {
+	Id         int64
+	Question   string
+	Answer     string
+	ContentsId ContentsId
+}
