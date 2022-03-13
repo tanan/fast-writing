77b0f9b5d83978a5fe590509232986e3ec3d9544
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-03-13 10:54:45 +0900

    fix dockerfile

--- environments/apps/fast-writing-db/build_and_push.sh
+++ environments/apps/fast-writing-db/build_and_push.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# DOCKER_BUILDKIT=1
-# export DOCKER_BUILDKIT
-
 app="fast-writing-db"
 tag=${1:-"latest"}
 image_name="gcr.io/anan-project/fast-writing/${app}:${tag}"
 
-# docker_dir=$(cd $(dirname $0); pwd)
-# app_dir="$docker_dir/../../../apps/${app}"
-
 build () {
-    # cd $app_dir
-    # docker build -f "${docker_dir}/Dockerfile" -t "${image_name}" .
     docker build -t "${image_name}" .
 }
 
@@ -20,5 +12,5 @@ push () {
     docker push $image_name
 }
 
-#build && push
+build && push
 build
