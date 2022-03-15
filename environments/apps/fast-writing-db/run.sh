f4c373561979a53440f26118ba1c325a20bbe8e4
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-03-16 08:45:07 +0900

    db: fix image

--- environments/apps/fast-writing-db/run.sh
+++ environments/apps/fast-writing-db/run.sh
@@ -2,7 +2,7 @@
 
 app="fast-writing-db"
 tag=${1:-"latest"}
-image_name="gcr.io/anan-project/fast-writing/${app}:${tag}"
+image_name="asia-northeast1-docker.pkg.dev/anan-project/fast-writing/${app}:${tag}"
 
 [[ `docker ps -a -f name=${app} -q | wc -l` -eq 1 ]] && docker rm -f ${app}
 docker run -d -p 3306:3306 \
