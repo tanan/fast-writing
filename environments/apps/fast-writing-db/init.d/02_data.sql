e7e781ee31ca4538cb6058eeac0d8578b39a905e
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-03-21 18:00:45 +0900

    add data

--- environments/apps/fast-writing-db/init.d/02_data.sql
+++ environments/apps/fast-writing-db/init.d/02_data.sql
@@ -6,3 +6,9 @@ insert into user values
 insert into writing_contents values
 (1, UUID_TO_BIN('11eae085-55e6-e2ca-a15d-0242ac110002'), 'title1', CURRENT_TIMESTAMP())
 ;
+
+insert into quiz values
+(1, 1, "私は誰ですか？", "Who am I?", CURRENT_TIMESTAMP()),
+(2, 1, "今日は晴れています。", "It is fine today.", CURRENT_TIMESTAMP()),
+(3, 1, "今朝ロンドンは雨が降っています。", "It is rainy in London in the morning today.", CURRENT_TIMESTAMP())
+;
