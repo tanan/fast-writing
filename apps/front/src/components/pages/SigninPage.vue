c6cf967326a84adce322ad3ac95295c59cbfff94
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-04-06 20:01:21 +0900

    add signin

--- apps/front/src/components/pages/SigninPage.vue
+++ apps/front/src/components/pages/SigninPage.vue
@@ -10,17 +10,17 @@
             <v-form>
               <div class="text-field">
                 <mdicon class="icon" name="account" :width="30" :height="30" />
-                <v-text-field name="login" label="Login" type="text"></v-text-field>
+                <v-text-field name="email" v-model="email" label="Email" type="text"></v-text-field>
               </div>
               <div class="text-field">
                 <mdicon class="icon" name="lock" :width="30" :height="30" />
-                <v-text-field id="password" name="password" label="Password" type="password"></v-text-field>
+                <v-text-field id="password" name="password" v-model="password" label="Password" type="password"></v-text-field>
               </div>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" to="/">Login</v-btn>
+            <v-btn color="primary" @click="signin(this.email, this.password)">Login</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -29,8 +29,44 @@
 </template>
 
 <script>
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgSiRqYgLaqNpg_Agn1u5kocxIAVB4OU8",
+  authDomain: "anan-project.firebaseapp.com",
+  databaseURL: "https://anan-project.firebaseio.com",
+  projectId: "anan-project",
+  storageBucket: "anan-project.appspot.com",
+  messagingSenderId: "474794270878",
+  appId: "1:474794270878:web:0a2bcc65d7f97da62e5b8f"
+};
+
+const app = initializeApp(firebaseConfig);
+
 export default {
-   name: 'SigninPage'
+   name: 'SigninPage',
+   data: () => ({
+    email: "",
+    password: "",
+  }),
+  methods: {
+    signin(email, password) {
+      console.log(email)
+      const auth = getAuth(app);
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user)
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode)
+          console.log(errorMessage)
+        });
+     }
+   }
 };
 </script>
 
