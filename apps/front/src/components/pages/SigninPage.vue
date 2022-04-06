5276bb85de6b0b280c2c4c6b4caea5b21f36b719
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-04-06 18:29:32 +0900

    create signin page

---
+++ apps/front/src/components/pages/SigninPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container class="signin" fluid fill-height>
+    <v-row justify="center">
+      <v-col cols="10" sm="4" md="3">
+        <v-card>
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Login form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <div class="text-field">
+                <mdicon class="icon" name="account" :width="30" :height="30" />
+                <v-text-field name="login" label="Login" type="text"></v-text-field>
+              </div>
+              <div class="text-field">
+                <mdicon class="icon" name="lock" :width="30" :height="30" />
+                <v-text-field id="password" name="password" label="Password" type="password"></v-text-field>
+              </div>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" to="/">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+   name: 'SigninPage'
+};
+</script>
+
+<style lang="scss">
+  .signin {
+    margin-top: 10%;
+    
+    .text-field {
+      display: flex;
+      .icon {
+        padding-right: 6px;
+        padding-top: 12px;
+        &:before {
+          content: "";
+        }
+      }
+    }
+  }
+</style>
--- apps/front/src/router/index.js
+++ apps/front/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TopPage from '../components/pages/TopPage.vue'
+import SigninPage from '../components/pages/SigninPage.vue'
 import WritingQuizPage from '../components/pages/WritingQuizPage.vue'
 import CreateQuizPage from '../components/pages/CreateQuizPage.vue'
 
@@ -8,6 +9,11 @@ const routes = [
     path: '/',
     name: 'TopPage',
     component: TopPage
+  },
+  {
+    path: '/signin',
+    name: 'SigninPage',
+    component: SigninPage
   },
   {
     path: '/contents/:id',
