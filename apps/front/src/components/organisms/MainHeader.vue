3a106a959ffd4c33e9554239f8ce5a1ed2703794
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-04-08 20:53:20 +0900

    wip header

--- apps/front/src/components/organisms/MainHeader.vue
+++ apps/front/src/components/organisms/MainHeader.vue
@@ -4,6 +4,26 @@
       <v-row no-gutters>
         <v-col cols="12" sm="3"><MainLogo /></v-col>
         <v-col cols="10" offset="1" sm="4" class="d-flex align-center"><HeaderForm /></v-col>
+        <v-spacer></v-spacer>
+        <v-col>
+          <div class="hamburger-menu">
+            <input type="checkbox" id="menu-btn-check">
+            <label for="menu-btn-check" class="menu-btn"><span></span></label>
+            <div class="menu-content">
+              <ul>
+                  <li>
+                      <a href="#">メニューリンク1</a>
+                  </li>
+                  <li>
+                      <a href="#">メニューリンク2</a>
+                  </li>
+                  <li>
+                      <a href="#">メニューリンク3</a>
+                  </li>
+              </ul>
+            </div>
+          </div>
+        </v-col>
       </v-row>
     </v-container>
   </div>    
@@ -29,5 +49,96 @@ export default {
 @import "@/styles/_variables.scss";
 .header {
   background-color: #55c500;
+}
+
+.menu-btn {
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  display: flex;
+  height: 64px;
+  width: 64px;
+  justify-content: center;
+  align-items: center;
+  z-index: 90;
+  
+  span:before {
+    bottom: 8px;
+  }
+
+  span:after {
+    top: 8px;
+  }
+  
+  span, span:before, span:after {
+    content: '';
+    display: block;
+    height: 3px;
+    width: 25px;
+    border-radius: 3px;
+    background-color: #ffffff;
+    position: absolute;
+  }
+}
+
+#menu-btn-check:checked ~ .menu-btn span {
+    background-color: rgba(255, 255, 255, 0);/*メニューオープン時は真ん中の線を透明にする*/
+}
+#menu-btn-check:checked ~ .menu-btn span::before {
+    bottom: 0;
+    transform: rotate(45deg);
+}
+#menu-btn-check:checked ~ .menu-btn span::after {
+    top: 0;
+    transform: rotate(-45deg);
+}
+
+#menu-btn-check {
+    display: none;
+}
+
+.menu-content {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 100%;
+  z-index: 80;
+  background-color: #3584bb;
+  transition: all 0.5s;
+
+  ul {
+    padding: 70px 10px 0;
+  }
+  ul li {
+    border-bottom: solid 1px #ffffff;
+    list-style: none;
+  }
+  ul li a {
+    display: block;
+    width: 100%;
+    font-size: 15px;
+    box-sizing: border-box;
+    color:#ffffff;
+    text-decoration: none;
+    padding: 9px 15px 10px 0;
+    position: relative;
+
+    &:before {
+      content: "";
+      width: 7px;
+      height: 7px;
+      border-top: solid 2px #ffffff;
+      border-right: solid 2px #ffffff;
+      transform: rotate(45deg);
+      position: absolute;
+      right: 11px;
+      top: 16px;
+    }
+  }
+}
+
+#menu-btn-check:checked ~ .menu-content {
+    left: 0;/*メニューを画面内へ*/
 }
 </style>
