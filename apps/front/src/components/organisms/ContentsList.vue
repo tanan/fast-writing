033a633d98444d78beb6dff3acf007a0ecd25bf1
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-04-17 13:04:56 +0900

    wip

--- apps/front/src/components/organisms/ContentsList.vue
+++ apps/front/src/components/organisms/ContentsList.vue
@@ -86,7 +86,7 @@ export default defineComponent({
             console.log(err)
             reject(err)
           }
-          console.log(resp.toObject())
+          console.log(resp)
           resolve(resp.toObject().contentslistList)
         })
       })
