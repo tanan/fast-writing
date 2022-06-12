ba8b0ca04f7b1bface4985053952bced4194862d
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-06-12 14:14:13 +0900

    add job deploy

--- apps/push-app/src/main.rs
+++ apps/push-app/src/main.rs
@@ -1,4 +1,6 @@
+use std::env;
 use diesel::prelude::*;
+use serde_json::{json, Value};
 use push_app::establish_connection;
 use push_app::models::{User, WritingContents};
 use push_app::schema::user as user_schema;
@@ -6,8 +8,8 @@ use push_app::schema::writing_contents as wc_schema;
 #[macro_use] extern crate diesel;
 
 mod schema;
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let connection = establish_connection();
     let users = user_schema::dsl::user
         .load::<User>(&connection)
@@ -20,7 +22,28 @@ fn main() {
         .load::<WritingContents>(&connection)
         .expect("Error loading users");
 
-    for c in contents {
-        println!("{:?}", c)
+    for content in contents {
+        println!("{:?}", content);
     }
+
+    let attachment = build_message(format!("https://front-cmfot4feta-an.a.run.app/contents/{}", 2));
+
+    let body = json!({
+        "channel": "tanan-notice1",
+        "attachments": vec![attachment],
+    });
+
+    let slack_url = env::var("SLACK_URL").expect("slack url must be set.");
+    let client = reqwest::Client::new();
+    client.post(slack_url).body(body.to_string()).send().await?;
+
+    Ok(())
+}
+
+fn build_message(contents_url: String) -> Value {
+    let text = format!("今日のクイズ：{}", contents_url);
+    return json!({
+        "text": text,
+        "color": "good",
+    })
 }
