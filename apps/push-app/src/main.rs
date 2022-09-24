0b24f267bb03e5006500e4f7b72053a6b804848d
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-09-24 18:57:21 +0900

    fix dynamic content

--- apps/push-app/src/main.rs
+++ apps/push-app/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use rand::Rng;
 use diesel::prelude::*;
 use serde_json::{json, Value};
 use push_app::establish_connection;
@@ -10,35 +11,42 @@ use push_app::schema::writing_contents as wc_schema;
 mod schema;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     let connection = establish_connection();
     let users = user_schema::dsl::user
+        .filter(user_schema::email.eq("toshifumi.anan@gmail.com"))
+        .limit(1)
         .load::<User>(&connection)
         .expect("Error loading users");
+
     for user in users {
-        println!("{:?}", user)
+        let contents = find_contents_by_user_id(user, &connection);
+
+        let num = rand::thread_rng().gen_range(0..contents.len());
+
+        let attachment = build_message(format!("https://front-cmfot4feta-an.a.run.app/contents/{}", &contents[num].id));
+
+        let body = json!({
+            "channel": "tanan-notice1",
+            "attachments": vec![attachment],
+        });
+
+        let slack_url = env::var("SLACK_URL").expect("slack url must be set.");
+        let client = reqwest::Client::new();
+        let res = client.post(slack_url).body(body.to_string()).send().await?;
+        println!("{}", &res.text().await?);
     }
-
-    let contents = wc_schema::dsl::writing_contents
-        .load::<WritingContents>(&connection)
-        .expect("Error loading users");
-
-    for content in contents {
-        println!("{:?}", content);
-    }
-
-    let attachment = build_message(format!("https://front-cmfot4feta-an.a.run.app/contents/{}", 2));
-
-    let body = json!({
-        "channel": "tanan-notice1",
-        "attachments": vec![attachment],
-    });
-
-    let slack_url = env::var("SLACK_URL").expect("slack url must be set.");
-    let client = reqwest::Client::new();
-    let res = client.post(slack_url).body(body.to_string()).send().await?;
-    println!("{}", &res.text().await?);
 
     Ok(())
+
+
+}
+
+fn find_contents_by_user_id(user: User, connection: &MysqlConnection) -> Vec<WritingContents> {
+    return wc_schema::dsl::writing_contents
+        .filter(wc_schema::user_id.eq(user.id))
+        .load::<WritingContents>(connection)
+        .expect("Error loading users");
 }
 
 fn build_message(contents_url: String) -> Value {
--- environments/endpoints/README.md
+++ environments/endpoints/README.md
@@ -1,2 +1,6 @@
-## docker build
-./gcloud_build_image -s fw-grpc-endpoint-cmfot4feta-an.a.run.app -c 2022-04-30r0 -p anan-project
+## USAGE
+
+```
+gcloud endpoints services deploy api_descriptor.pb api_config.yaml
+./gcloud_build_image -s fw-grpc-endpoint-cmfot4feta-an.a.run.app -c [configuration tag] -p anan-project
+```
